f835e8d32a1cfac2603fc8f50880546806252c2f
Cesar Alcancio <cesar.alcancio@payclip.com>
2019-11-29 22:56:28 -0600

    navegando diferente timeline

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -3,21 +3,38 @@ import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {fotos:[]};
+        this.login = this.props.login;
     }
 
-    componentDidMount() {
+    carregaFotos() {
         console.log('componentDidMount init')
         const token = localStorage.getItem('auth-token')
-        // const url = 'http://localhost:8080/api/fotos?X-AUTH-TOKEN=' + token
-        const url = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${token}`
+        const login = this.login;
+        let url = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${token}`
+
+        if (login !== undefined) {
+            url = `http://localhost:8080/api/public/fotos/${login}`
+        }
+
         fetch(url)
             .then(response => response.json())
             .then(fotos => {
                 this.setState({fotos:fotos});
             });
+    }
+
+    componentDidMount() {
+        this.carregaFotos();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.login !== undefined) {
+            this.login = nextProps.login;
+            this.carregaFotos();
+        }
     }
 
     render() {
